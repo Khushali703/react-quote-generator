313c7bfc3ef8f0ce020e18d98e8731dc706e5602
Khushali703 <tankkhushali1@gmail.com>
2025-05-03 07:52:26 +0530

    update the routing code

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,46 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./components/header";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import Quotes from "./components/quotes";
+import FavItems from "./components/favItems";
 
 function App() {
   return (
     <>
       <div className="container">
-        <Header />
-        <Outlet />
+        <Router>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Header />
+                  <Quotes />
+                </>
+              }
+            />
+            <Route
+              path="/quotes"
+              element={
+                <>
+                  <Header />
+                  <Quotes />
+                </>
+              }
+            />
+            <Route
+              path="/favourite"
+              element={
+                <>
+                  <Header />
+                  <FavItems />
+                </>
+              }
+            />
+          </Routes>
+        </Router>
       </div>
     </>
   );
